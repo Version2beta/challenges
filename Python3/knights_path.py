3a742e8e4eeeb16d2fa848f39a4c4014735f9da2
Rob Martin <rob.martin@packsize.com>
2020-05-10 04:07:54 -0600

    fixed

--- Python3/knights_path.py
+++ Python3/knights_path.py
@@ -16,7 +16,7 @@ def valid_moves(board, origin):
         (-2, 1),
         (-1, 2)
     ]
-    origin_rank, origin_file = origin
+    origin_file, origin_rank = origin
     all_moves = [
         (origin_file + file_offset, origin_rank + rank_offset) for file_offset, rank_offset in patterns
     ]
@@ -34,18 +34,23 @@ def lookup_notation(board, location):
     return invert.get(location)
 
 
-def find_path(board, origin, destination, depth, path=[]):
-    node = Tree(lookup_notation(board, origin))
-    my_path = path[:]
-    my_path.append(origin)
+def find_path(board, origin, destination, depth, paths=dict(), path=tuple()):
+    path = path + tuple([lookup_notation(board, origin)])
     if origin == destination:
-        return Tree(lookup_notation(board, destination), True)
-    if len(path) >= depth:
-        node.children = False
-        return node
-    node.children = [find_path(board, hop, destination, depth, my_path)
-                     for hop in valid_moves(board, origin) if hop not in my_path]
-    return node
+        paths[path] = True
+        path = ()
+    elif depth < len(path):
+        path = ()
+    else:
+        hops = valid_moves(board, origin)
+        for hop in hops:
+            if lookup_notation(board, hop) not in path:
+                paths = find_path(board, hop, destination, depth, paths, path)
+    return paths
+
+
+def shortest_path(root):
+    return sorted(list(root), key=len)[0]
 
 
 def main(origin, destination, depth=20):
@@ -53,25 +58,4 @@ def main(origin, destination, depth=20):
     origin = lookup_coords(board, origin)
     destination = lookup_coords(board, destination)
     root = find_path(board, origin, destination, depth)
-    # root.purge()
-    return root
-
-
-class Tree(object):
-    def __init__(self, label, children=[]):
-        self.label = label
-        self.children = children
-
-    def __repr__(self):
-        return "%s -> %r" % (self.label, self.children)
-
-    def paths(self, paths=[], path=[]):
-        path.append(self.label)
-        if self.children == True:
-            paths.append(path)
-            path = []
-            return paths
-        if self.children == False:
-            path = []
-            return paths
-        return [child.paths(paths, path]) for child in self.children]
+    return sorted(list(root), key=len)
